ec0f14f24ecc5e48d43c22d5fcea6d6decebbdaf
cxreiff <cooper@cxreiff.com>
2016-07-16 05:34:48 -0700

    added: touch control, color changing, app icon

--- Kayv/GameScene.swift
+++ Kayv/GameScene.swift
@@ -15,9 +15,12 @@ class GameScene: SKScene {
     static let frequency: CGFloat = 6.0
     static let duration: CGFloat = 4.0
     
-    var initialTilt: CGFloat?
-    
+    private var initialTilt: CGFloat?
     private var lastUpdateTime : TimeInterval = 0
+    private var timeElapsedSinceSpawn: TimeInterval = 0
+    private var isTransitioning: Bool = false
+    
+    private var targetTouch: UITouch?
     
     private var entityManager: EntityManager!
     private var cam: SKCameraNode!
@@ -45,16 +48,22 @@ class GameScene: SKScene {
         self.corridor.particleTexture = SKTexture(imageNamed: "corridor")
         self.corridor.particleBirthRate = GameScene.frequency
         self.corridor.particleLifetime = GameScene.duration
+        self.corridor.particleColorBlendFactor = 1.0
+        self.corridor.particleColor = SKColor.white()
         self.corridor.particleAlpha = 0.0
         self.corridor.particleAlphaSpeed = 1.0 / GameScene.duration
         self.corridor.particleScaleSpeed = 4.0 / GameScene.duration
         
         self.crosshairX = SKSpriteNode(imageNamed: "crosshairX")
         self.crosshairY = SKSpriteNode(imageNamed: "crosshairY")
+        self.crosshairX.color = SKColor.white()
+        self.crosshairY.color = SKColor.white()
         self.crosshairX.size = CGSize(width: 4, height: self.frame.height * 2.0)
         self.crosshairY.size = CGSize(width: self.frame.width * 2.0, height: 4)
-        self.crosshairX.constraints = [SKConstraint.positionX(SKRange(lowerLimit: self.frame.minX, upperLimit: self.frame.maxX))]
-        self.crosshairY.constraints = [SKConstraint.positionY(SKRange(lowerLimit: self.frame.minY, upperLimit: self.frame.maxY))]
+        self.crosshairX.constraints = [SKConstraint.positionX(SKRange(lowerLimit: self.frame.minX,
+                                                                      upperLimit: self.frame.maxX))]
+        self.crosshairY.constraints = [SKConstraint.positionY(SKRange(lowerLimit: self.frame.minY,
+                                                                      upperLimit: self.frame.maxY))]
         
         self.motionManager = CMMotionManager()
         self.motionManager.startAccelerometerUpdates()
@@ -67,23 +76,20 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            print(location)
-        }
+        targetTouch = touches.first!
+        
+        colorShift(red: 0.0, green: 1.0, blue: 1.0, duration: 4.0)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            print(location)
-        }
+
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.location(in: self)
-            print(location)
+            if touch == targetTouch { targetTouch = nil }
+            
+            colorShift(red: 0.0, green: 0.0, blue: 0.0, duration: 4.0)
         }
     }
     
@@ -96,12 +102,39 @@ class GameScene: SKScene {
         
         self.lastUpdateTime = currentTime
         
-        if let tiltData = motionManager.accelerometerData {
-            if initialTilt == nil { self.initialTilt = CGFloat(tiltData.acceleration.y) }
-            crosshairX?.position.x += CGFloat(tiltData.acceleration.x) * 16
-            crosshairY?.position.y += (CGFloat(tiltData.acceleration.y) - initialTilt!) * 16
+        if let location = targetTouch?.location(in: self) {
+            crosshairX.position.x += (location.x - crosshairX.position.x) * 0.4
+            crosshairY.position.y += (location.y - crosshairY.position.y) * 0.4
         }
         
+//        if let tiltData = motionManager.accelerometerData {
+//            if initialTilt == nil { self.initialTilt = CGFloat(tiltData.acceleration.y) }
+//            crosshairX?.position.x += CGFloat(tiltData.acceleration.x) * 16
+//            crosshairY?.position.y += (CGFloat(tiltData.acceleration.y) - initialTilt!) * 16
+//        }
+        
+//        self.timeElapsedSinceSpawn += dt
+//        if timeElapsedSinceSpawn > 30 {
+//            self.addChild(SKSpriteNode(imageNamed: "corridor"))
+//            self.timeElapsedSinceSpawn = 0
+//        }
+        
+        if isTransitioning { corridor.particleColor = crosshairX.color }
+        
         cam.position = CGPoint(x: crosshairX.position.x / 4.0, y: crosshairY.position.y / 8.0)
+    }
+    
+    private func colorShift(red: CGFloat, green: CGFloat, blue: CGFloat, duration: TimeInterval) {
+        isTransitioning = true
+        let background = SKColor(red: red, green: green, blue: blue, alpha: 1.0)
+        self.run(SKAction.sequence([SKAction.colorize(with: background, colorBlendFactor: 1.0, duration: duration),
+                                    SKAction.run({ self.isTransitioning = false })]))
+        if (red + green + blue > 1.5) {
+            crosshairX.run(SKAction.colorize(with: SKColor.black(), colorBlendFactor: 1.0, duration: duration))
+            crosshairY.run(SKAction.colorize(with: SKColor.black(), colorBlendFactor: 1.0, duration: duration))
+        } else {
+            crosshairX.run(SKAction.colorize(with: SKColor.white(), colorBlendFactor: 1.0, duration: duration))
+            crosshairY.run(SKAction.colorize(with: SKColor.white(), colorBlendFactor: 1.0, duration: duration))
+        }
     }
 }
